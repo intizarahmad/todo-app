3042a81867978fd867b4eff7ddb294a2ce00ac4a
Initzar Ahmad <intizarahmad@gmail.com>
2018-11-02 11:37:37 +0530

    mlab string

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-//mongoose.connect('mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true });
-mongoose.connect('mongodb://intizar:todo12345@ds149593.mlab.com:49593/todos',{ useNewUrlParser: true });
+//mongoose.connect('',{ useNewUrlParser: true });
+const dbstring = process.env.MLAB_MONGO_KEYS ||'mongodb://localhost:27017/TodoApp';
+mongoose.connect(dbstring,{ useNewUrlParser: true });
 
 module.exports = {
     mongoose
