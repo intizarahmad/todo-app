3a128723dda2c69de41e8977dd30a17c1734b6b3
Initzar Ahmad <intizarahmad@gmail.com>
2018-11-02 09:23:02 +0530

    Add get tods/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const ObjectId = require('mongoose').Types.ObjectId;
 const { mongoose } = require('../db/mongoose');
 const { UserModel, TodoModel } = require('../models');
 
@@ -26,6 +27,23 @@ app.get('/todos', (req, res)=>{
         res.status(400).send(e);
      });
  })
+
+ app.get('/todos/:id', (req, res)=>{
+     let id = req.params.id; 
+     if(!ObjectId.isValid(id)){
+        res.status(404).send();
+     }
+    TodoModel.findById(id).then(todo=>{
+        if(!todo){
+            res.status(404).send();
+        }
+        res.send({
+            todo
+        })
+    }).catch(e=>{
+       res.status(404).send();
+    });
+})
 
 
 app.listen(3000, ()=>{
